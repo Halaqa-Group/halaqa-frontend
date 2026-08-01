c9a6991e4020f82b4345d07fdf25d73c98eb24e8
husamMousa <88555163+husamMousa@users.noreply.github.com>
2026-08-01 20:59:21 +0300

    Update app.vue

--- app/app.vue
+++ app/app.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import * as locales from '@nuxt/ui/locale'
 
-const { locale } = useI18n()
+const { locale } = useI18n();
 
 const lang = computed(() => locales[locale.value].code)
 const dir = computed(() => locales[locale.value].dir)
